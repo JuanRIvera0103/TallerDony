5ba0ed8649a377ffec3aa54f4401e73004f773d8
JuanRivera0103 <juanriverarestrepo0103@gmail.com>
2020-07-31 23:25:38 -0500

    Dentro de la carpeta se agrego lo de molina

---
+++ TallerDony/Lo de molina/Detalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace supermercado.Entidades
+{
+    class Detalle
+    {
+        public int IdDetalle { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public double Precio { get; set; }
+
+        public Detalle( string nombre, int cantidad, double precio)
+        {
+         
+            this.Nombre = nombre;
+            this.Cantidad = cantidad;
+            this.Precio = precio;
+        }
+
+    }
+}
---
+++ TallerDony/Lo de molina/Factura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace supermercado.Entidades
+{
+    class Factura
+    {
+        public int IdFactura { get; set; }
+        public int IdCliente { get; set; }
+        public string Fecha { get; set; }
+        public int productos { get; set; }
+
+    }
+}
