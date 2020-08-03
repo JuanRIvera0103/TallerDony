f48b56710dbabb3c50862bdf9fcb1679370e6ee6
TheBoreOne <69027969+TheBoreOne@users.noreply.github.com>
2020-08-02 19:48:55 -0500

    Todo Full HD sino le sirve es problema suyo xd
    Se añadió Config

---
+++ Dony/Dony/Servicios/Config.cs
@@ -0,0 +1,37 @@
+﻿using Dony.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Dony.Servicios
+{
+    class Config
+    {
+        public void Leer10Clientes()
+        {
+            StreamReader sr = new StreamReader(@"Cliente10.txt");
+            ServicioCliente servicioCliente = new ServicioCliente();
+            string input = "";
+            while (input != null) { 
+            input = sr.ReadLine();
+            if (input != null) { 
+            List<String> elements = input.Split(',').ToList();
+            string cedula = elements[0];
+            string nombre = elements[1];
+            string direccion = elements[2];
+            string telefono = elements[3];
+            Cliente cliente = new Cliente(cedula, nombre, direccion, telefono);
+                    servicioCliente.AgregarCliente10(cliente, cedula);
+            elements.Clear();
+        }
+
+        }
+            
+            sr.Close();
+            servicioCliente.Validad10();
+        }
+    }
+}
